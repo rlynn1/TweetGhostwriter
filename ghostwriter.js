0b363e11f84778a23d9984c120a17abcce638707
rlynn1 <rlynn1@mail.csuchico.edu>
2017-04-06 14:59:35 -0700

    RSG
    Created a random sentence generator.

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,20 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intros = ["You know those people who", "Today I really want to",
+                "Have you ever tried to", "Sometimes I wonder if my descendants will"];
+  var body = ["party all day and then go home and cry", "eat an entire pizza in one sitting",
+              "only play laser tag against little kids to ensure victory",
+              "do my math homework", "learn how to bake"];
+  var end = ["all for my glory.", "in an attempt to find meaning in life."];
+  randIndex = randomUpTo(intros.length-1);
+  sentence = intros[randIndex] + " ";
+  randIndex = randomUpTo(body.length-1);
+  sentence += body[randIndex] + " ";
+  randIndex = randomUpTo(end.length-1);
+  sentence += end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
